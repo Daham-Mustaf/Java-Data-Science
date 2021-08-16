97243da4efc525a9f9cc6e0f3f1fb3e7c8733c85
Daham-Mustaf <s7damust@uni-bonn.de>
2021-08-16 11:45:58 +0200

    The the output from the API is added

--- src/dataHandling/LoadingData.java
+++ src/dataHandling/LoadingData.java
@@ -3,6 +3,8 @@ package dataHandling;
 import tech.tablesaw.api.Table;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 
 public class LoadingData {
 
@@ -21,6 +23,23 @@ public class LoadingData {
         Table structure = dataTable.structure();
         System.out.println("the structure of hr_comma_.csv ?");
         System.out.println(structure);
+
+        /**
+         * Loading the data from the API by using the Stream API function
+         * Creating a function  to load data from  Stream API
+         * The data has to be passed through the Stream Interface
+         * the link to where the data is located.
+         */
+
+        // https://github.com/Daham-Mustaf/Java-Data-Science/blob/main/HR_comma_sep.csv
+
+        String location =  "https://github.com/Daham-Mustaf/Java-Data-Science/blob/main/HR_comma_sep.csv";
+        Table hrDataTable;
+        try (InputStream input = new URL(location).openStream()) {
+            hrDataTable = Table.read().csv(input, "bush");
+        }
+        Table streamStructure = hrDataTable.structure();
+        System.out.println("the output from the API"+ streamStructure);
 
         }
     }
