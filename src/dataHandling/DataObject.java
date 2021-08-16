9fbcaa1cc842c0961bff2c40a27b24ffb821d80c
Daham-Mustaf <s7damust@uni-bonn.de>
2021-08-16 08:48:27 +0200

    the update of Rows and columns handling

--- src/dataHandling/DataObject.java
+++ src/dataHandling/DataObject.java
@@ -1,6 +1,8 @@
 package dataHandling;
 
 import java.io.IOException;
+import tech.tablesaw.api.DoubleColumn;
+import tech.tablesaw.api.StringColumn;
 import tech.tablesaw.api.Table;
 
 public class DataObject {
@@ -27,12 +29,55 @@ public class DataObject {
 
 		/**
 		 *Knowing the size of the table that loaded.
-		 *Knowing the columns and the rows
+		 *Knowing the number and the size of (columns, rows)
 		 */
 
-		System.out.println("the total number of columns and rows");
-		String tableShape = hrAnalytics.shape();
+		System.out.println("Total number of columns and rows");
+		String tableShape = dataTable.shape();
 		System.out.println(tableShape);
+
+		//Creating a new table as a subset of original table.
+		Table newSubTable = dataTable.select( "satisfaction_level", "last_evaluation", "number_project", "sales", "salary");
+		System.out.println(newSubTable.columnNames());
+
+		//combining tables: combination based on rows
+		//Table mergedTables1 = dataTable.append(newSubTable);
+		//Combining  based on columns
+		//Table mergedTables2 = newSubTable.concat(dataTable);
+
+		/**
+		 *Handling of Columns
+		 */
+
+		//Printing all the column names
+		System.out.println(dataTable.columnNames());
+
+		//Retrieving a single column from the table
+
+		StringColumn theSalary = (StringColumn) dataTable.column("salary");
+		System.out.println(theSalary);
+
+		//Selecting the columns that you want want to sort on.
+		//Sorting the table with specific Columns( ascending or descending manner)
+		Table ascendingHr = dataTable.sortAscendingOn("salary");
+		System.out.println(ascendingHr.first(5));;
+
+		/**
+		 * Interacting with rows
+		 */
+
+		//Accessing the first 3 rows of the table
+		System.out.println("First rows of the table");
+		Table tableHead = dataTable.first(3);
+		System.out.println(tableHead);
+
+		//Accessing the last 3 rows of the table
+		System.out.println("Last rows of the table");
+		Table tableTail = dataTable.last(3);
+		System.out.println(tableTail);
+
+
+
 
 	}
 }
