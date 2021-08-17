cc7b9ebc72f52689bf1500aec937a6d7a9e1eea0
Daham-Mustaf <s7damust@uni-bonn.de>
2021-08-17 14:12:34 +0200

    TimeSeriesPlot implimented

--- src/dataHandling/Trend.java
+++ src/dataHandling/Trend.java
@@ -1,19 +1,29 @@
 package dataHandling;
 
 import tech.tablesaw.api.Table;
-
 import java.io.IOException;
+import tech.tablesaw.plotly.Plot;
+import tech.tablesaw.plotly.api.TimeSeriesPlot;
 
+/**
+ * Trend analysis is trying to recognize patterns from data that has been collected
+ * overtime, predicting features using historical data.
+ * features are characteristics being observed in the data.
+ */
 public class Trend {
     public Trend(){}
 
 
     public static void main(String[] args) throws IOException {
         Trend myTrend = new Trend();
-        Table diabetesData = Table.read().csv("/Users/m-store/eclipse-workspace/DataAnalysis/Bitcoin-usd.csv");
-        Table diabetes = diabetesData.structure();
-        System.out.println(diabetes);
+        Table bitconPriceData = Table.read().csv("/Users/m-store/eclipse-workspace/DataAnalysis/Bitcoin-usd.csv");
+        Table bitprice = bitconPriceData.structure();
+        System.out.println(bitprice);
 
-
+        //Time series data trend Analysis
+        // create(title, TableData, key Features x & y axis).
+        // Plot.show prints out the plot
+        Plot.show(TimeSeriesPlot
+                .create("Bitcoin Exchange Prices in USD", bitconPriceData, "Date", "High"));
     }
 }
