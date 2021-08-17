f83fde6a414d144717dd0798b7784b576378d350
Daham-Mustaf <s7damust@uni-bonn.de>
2021-08-17 14:57:37 +0200

    HorizontalBarPlot & PiePlot

--- src/dataHandling/Trend.java
+++ src/dataHandling/Trend.java
@@ -3,7 +3,10 @@ package dataHandling;
 import tech.tablesaw.api.Table;
 import java.io.IOException;
 import tech.tablesaw.plotly.Plot;
+import tech.tablesaw.plotly.api.HorizontalBarPlot;
+import tech.tablesaw.plotly.api.PiePlot;
 import tech.tablesaw.plotly.api.TimeSeriesPlot;
+import static tech.tablesaw.aggregate.AggregateFunctions.*;
 
 /**
  * Trend analysis is trying to recognize patterns from data that has been collected
@@ -25,5 +28,39 @@ public class Trend {
         // Plot.show prints out the plot
         Plot.show(TimeSeriesPlot
                 .create("Bitcoin Exchange Prices in USD", bitconPriceData, "Date", "High"));
+
+        /**
+         * Visualizing different data forms
+         * Text (Word cloud)
+         * Number (bar charts)
+         */
+
+//Creating Bar Charts
+        Table diabetesData = Table.read().csv("/Users/m-store/eclipse-workspace/DataAnalysis/diabetes.csv");
+        Table diabetes = diabetesData.structure();
+        System.out.println(diabetes);
+        Table ageAverage = diabetesData.summarize("Age", mean).by("Outcome");
+        Plot.show(
+                HorizontalBarPlot.create(
+                        " Average Age by Outcome",		    // plot title
+                        ageAverage,				                // Calculated averages of the age
+                        "Outcome",					// grouping column name
+                        "mean [Age]"));
+
+         //Showcase AVG BMI by Age
+        Table bmiAverage = diabetesData.summarize("BMI", mean).by("Age");
+        Plot.show(
+                HorizontalBarPlot.create(
+                        " Average BMI by Age",	     	// plot title
+                        bmiAverage,				            // Calculated averages of the aGE
+                        "Age",					// grouping column name
+                        "mean [BMI]"));
+
+
+       //Using Pie Chart to showcase average age by IBM
+        Plot.show(
+                PiePlot.create("Average Age by SEX", ageAverage, "IBM", "mean [AGE]"));
+
+
     }
 }
